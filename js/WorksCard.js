4e5ab38efd88daadd5ad9993f58808a557434d2c
Muhammad Ismail <63145617+b1994mi@users.noreply.github.com>
2020-04-22 10:49:57 +0700

    Add OG meta tag & change link
    Add meta tag to improve incoming search & change the github link on works card.

--- js/WorksCard.js
+++ js/WorksCard.js
@@ -7,7 +7,7 @@ Vue.component('works-card', {
 
             works: [
                 { img: 'images/my-first.png', title: 'My Map App', content: "A simple web app using Django Framework. It extends the default User Model and can show a map using user's saved coordinate.", link: 'https://github.com/b1994mi/myfirstproject', logo: 'fab fa-github w3-large', logoText: 'Github' },
-                { img: 'images/my-second.png', margin: 16, title: 'My Portfolio Web', content: 'The website you are looking at right now. It uses Vue.js to make dark/light theme button on top-right, W3.css plus Font Awesome to make it beautiful, and Glider.js to make a card carousel.', link: '', logo: 'fab fa-github w3-large', logoText: 'Github' },
+                { img: 'images/my-second.png', margin: 16, title: 'My Portfolio Web', content: 'The website you are looking at right now. It uses Vue.js to make dark/light theme button on top-right, W3.css plus Font Awesome to make it beautiful, and Glider.js to make a card carousel.', link: 'https://github.com/b1994mi/b1994mi.github.io', logo: 'fab fa-github w3-large', logoText: 'Github' },
                 { img: 'images/my-third.png', title: 'Your Website', content: 'Your website? Yes, your website! I can make a portfolio for photographers/designers, a food menu, or even a simple blog using static web.', link: '#contacts', logo: 'fab fa-whatsapp w3-large', logoText: 'Contact Me' }
             ]
 
