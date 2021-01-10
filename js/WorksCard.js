f024decd2f9dd3378507d05f13a28bde4ddf5c94
b1994mi <b1994mi@gmail.com>
2021-01-10 10:28:12 +0700

    tmbh hasil project

--- js/WorksCard.js
+++ js/WorksCard.js
@@ -6,9 +6,23 @@ Vue.component('works-card', {
         return {
 
             works: [
-                { img: 'images/my-first.png', title: 'My Map App', content: "A simple web app using Django Framework. It extends the default User Model and can show a map using user's saved coordinate.", link: 'https://github.com/b1994mi/myfirstproject', logo: 'fab fa-github w3-large', logoText: 'Github' },
-                { img: 'images/my-second.png', margin: 16, title: 'My Portfolio Web', content: 'The website you are looking at right now. It uses Vue.js to make dark/light theme button on top-right, W3.css plus Font Awesome to make it beautiful, and Glider.js to make a card carousel.', link: 'https://github.com/b1994mi/b1994mi.github.io', logo: 'fab fa-github w3-large', logoText: 'Github' },
-                { img: 'images/my-third.png', title: 'Your Website', content: 'Your website? Yes, your website! I can make a portfolio for photographers/designers, a food menu, or even a simple blog using static web.', link: '#contacts', logo: 'fab fa-whatsapp w3-large', logoText: 'Contact Me' }
+                { img: 'images/my-fourth.png', title: 'SoccerUpdate', content: "Static web that consume Football-Data and Wikipedia REST API with modern Javascript syntax and Bootstrap. This was a group assignment when I took full-stack bootcamp", links: [
+                    {link: 'https://rahmatvp.github.io/projectIsmailRahmat/', logo: 'fa fa-desktop w3-large', logoText: 'Website'},
+                    {link: 'https://github.com/rahmatvp/projectIsmailRahmat', logo: 'fab fa-github w3-large', logoText: 'Github'},
+                ]},
+                { img: 'images/my-fifth.png', title: 'Redesign Lazada', content: "A bootcamp assignment using Bootstrap and pure Javascript. I was able to recreate the Lazada website, there is a search bar that can search the products and you can add the products into the cart.", links: [
+                    {link: 'http://rocky-retreat-58140.herokuapp.com/', logo: 'fa fa-desktop w3-large', logoText: 'Website'},
+                    {link: 'https://github.com/b1994mi/redesign-lazada', logo: 'fab fa-github w3-large', logoText: 'Github'},
+                ]},
+                { img: 'images/my-first.png', title: 'My Map App', content: "A simple web app using Django Framework. It extends the default User Model and can show a map using user's saved coordinate.", links: [
+                    {link: 'https://github.com/b1994mi/myfirstproject', logo: 'fab fa-github w3-large', logoText: 'Github'}
+                ]},
+                { img: 'images/my-second.png', margin: 16, title: 'My Portfolio Web', content: 'The website you are looking at right now. It uses Vue.js to make dark/light theme button on top-right, W3.css plus Font Awesome to make it beautiful, and Glider.js to make a card carousel.', links: [
+                    {link: 'https://github.com/b1994mi/b1994mi.github.io', logo: 'fab fa-github w3-large', logoText: 'Github'}
+                ]},
+                { img: 'images/my-third.png', title: 'Your Website', content: 'Your website? Yes, your website! I can make a portfolio for photographers/designers, a food menu, or even a simple blog using static web.', links: [
+                    {link: '#contacts', logo: 'fab fa-whatsapp w3-large', logoText: 'Contact Me'}
+                ]}
             ]
 
         }
@@ -29,8 +43,8 @@ Vue.component('works-card', {
                 <div :class="isDark? 'title-card' : 'title-card-light'">{{ work.title }}</div>
                 <p :class="isDark? 'body-card' : 'body-card-light'">{{ work.content }}</p>
                 <div>
-                    <p class="w3-green w3-round-xlarge w3-padding">
-                        <a :href="work.link"><i :class="work.logo"></i> {{ work.logoText }}</a>
+                    <p v-for="link in work.links" :key="link" class="w3-green w3-round-xlarge w3-padding">
+                        <a :href="link.link"><i :class="link.logo"></i> {{ link.logoText }}</a>
                     </p>
                 </div>
                 <br>
